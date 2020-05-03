a22711eecdb78b8f852aea3efa66a4698da7b813
DenysCheporniuk <denys.cheporniuk@gmail.com>
2020-05-03 20:46:54 +0300

    created database and models: user, team, channel, message

---
+++ models/message.js
@@ -0,0 +1,17 @@
+export default (sequelize, DataTypes) => {
+  const Message = sequelize.define('message', {
+    text: DataTypes.STRING,
+  });
+
+  Message.associate = (models) => {
+    Message.belongsTo(models.User, {
+      foreignKey: 'userId',
+    });
+
+    Message.belongsTo(models.Channel, {
+      foreignKey: 'channelId',
+    });
+  };
+
+  return Message;
+};
